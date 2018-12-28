c95696c1fb5f31af3981c0605e4facde7fcb1c88
Jerry <285373161@qq.com>
2018-12-28 19:07:59 +0800

    【init】build project

---
+++ app/src/main/java/com/jerry/customviewset/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jerry.customviewset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 该项目收集了一些常用自定义View
+ *
+ * @author xujierui
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
